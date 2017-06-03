fe4a2d58540a8787267000441a764a08a91807bf
wmmmm <1229819460@qq.com>
2017-06-03 21:58:22 +0800

    添加热量滚动轴

--- Assets/wangmeng/Scripts/HotScroll.cs
+++ Assets/wangmeng/Scripts/HotScroll.cs
@@ -1,26 +1,70 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using DG.Tweening;
+using UnityEngine.UI;
 
+
+public enum ScrollbarType
+{
+	adding,
+	pausing,
+	producting
+}
 public class HotScroll : MonoBehaviour {
 
+
+
+
 	// Use this for initialization
+    public ScrollbarType scrollbarType=ScrollbarType.pausing;
 	private float time;
-	public float addSpeed;
+	public float addSpeed=0.01f;
+    
+	public float productSpeed=0.02f;
+	private float number{
+		get {
+			return GetComponent<Scrollbar>().size;
+		}
+		set
+		{
+			GetComponent<Scrollbar>().size=value;
+		}
+	}
 	void Start () {
 		
 	}
 	
 	// Update is called once per frame
 	void Update () {
+		if(scrollbarType==ScrollbarType.pausing)
+		return;
+
+		if(scrollbarType==ScrollbarType.adding)
+		{
+			number+=addSpeed;
+		}
+        
+ 	     if(scrollbarType==ScrollbarType.producting)
+		{
+			number-=productSpeed;
+		}
+         
+        
+
+		if(number==1)
+	       Death();
+		
+        
 		
 	}
-    public void Addlength()
-	{
-     
-	}
-	public void ReductLength()
-	{
+   
+    
 
+	void Death()
+	{
+	   Debug.Log("失败");
 	}
+
+	
 }
