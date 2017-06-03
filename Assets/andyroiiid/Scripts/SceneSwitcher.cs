1942128f3555c5ea86d77ae2105daa5753815e67
Andrew Huang <andyroiiid@gmail.com>
2017-06-04 02:25:12 +0800

    场景异步加载、屏幕明暗变化

---
+++ Assets/andyroiiid/Scripts/SceneSwitcher.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using DG.Tweening;
+
+public class SceneSwitcher : MonoBehaviour
+{
+    public static float maskTime = 1.0f;
+
+    public ScreenMask mask;
+
+    private AsyncOperation _asyncOpertaion = null;
+
+    void Start()
+    {
+        // 进入场景时播放遮蔽从有到无
+        mask.Switch(false, maskTime);
+    }
+
+    public void Goto(string scene)
+    {
+        // 如果已经有场景在加载，就不要启动新的异步加载协程
+        if (_asyncOpertaion != null)
+            return;
+
+        // 启动异步加载协程
+        StartCoroutine(LoadSceneAsync(scene));
+
+        // 切换场景前播放遮蔽从无到有
+        mask.Switch(true, maskTime).OnComplete(() =>
+        {
+            // TODO: 这里认为 maskTime 的时间之内场景一定能加载完成
+            // 因为异步加载已经完成，这里应该能够瞬间切换
+            SceneManager.LoadScene(scene);
+        });
+    }
+
+    // 异步加载场景
+    IEnumerator LoadSceneAsync(string scene)
+    {
+        _asyncOpertaion = SceneManager.LoadSceneAsync(scene, LoadSceneMode.Single);
+        // 加载完成后等待遮蔽完成再切换场景
+        _asyncOpertaion.allowSceneActivation = false;
+        yield return _asyncOpertaion;
+    }
+}
---
+++ Assets/andyroiiid/Scripts/ScreenMask.cs
@@ -0,0 +1,62 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using DG.Tweening;
+
+[RequireComponent(typeof(Image))]
+public class ScreenMask : MonoBehaviour
+{
+    private Image _image;
+
+    // 控制 _image 透明度来操作遮蔽
+    public float Opacity
+    {
+        get { return _image.color.a; }
+        set
+        {
+            Color color = _image.color;
+            color.a = value;
+            _image.color = color;
+        }
+    }
+
+    void Awake()
+    {
+        _image = GetComponent<Image>();
+    }
+
+    private Tweener _tweener;
+
+    public Tweener Switch(bool on, float duration)
+    {
+        // 开启 gameObject
+        gameObject.SetActive(true);
+
+        if (_tweener != null)
+        {
+            // 如果已经在渐变，就立刻中止
+            _tweener.Kill();
+        }
+        else
+        {
+            // 设置 _image 透明度初始值
+            Opacity = on ? 0.0f : 1.0f;
+        }
+
+        // 设置 _image 透明度目标值
+        float endOpacity = on ? 1.0f : 0.0f;
+
+        _tweener = DOTween
+            .To(() => Opacity, (x) => Opacity = x, endOpacity, duration)
+            .OnComplete(() =>
+            {
+                // 关闭 gameObject
+                gameObject.SetActive(false);
+                // 清除 _tweener
+                _tweener = null;
+            });
+
+        return _tweener;
+    }
+}
