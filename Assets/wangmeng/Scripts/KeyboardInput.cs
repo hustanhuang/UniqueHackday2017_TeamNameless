a55ef2547d0ceea0cdcc90afe69083b64b833405
wmmmm <1229819460@qq.com>
2017-06-03 21:02:36 +0800

    添加拿起放下扇子的接口函数

---
+++ Assets/wangmeng/Scripts/KeyboardInput.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class KeyboardInput : MonoBehaviour {
+
+	// Use this for initialization
+	public KeyCode getFan;
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	    if (Input.GetKeyDown(getFan))
+        {
+            Debug.Log("拿起扇子");
+        } 
+        if (Input.GetKeyUp(getFan))
+        {
+            Debug.Log("放下扇子");
+        }
+     
+	}
+}
