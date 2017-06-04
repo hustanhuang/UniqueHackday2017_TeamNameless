793fab6c7ea203988118abd571e522a2abed466d
Andrew Huang <andyroiiid@gmail.com>
2017-06-04 10:12:14 +0800

    取消隐藏指针

--- Assets/wangmeng/Scripts/ChangeRadius.cs
+++ Assets/wangmeng/Scripts/ChangeRadius.cs
@@ -2,77 +2,81 @@
 using System.Collections.Generic;
 using UnityEngine;
 using DG.Tweening;
-public class ChangeRadius : MonoBehaviour {
+public class ChangeRadius : MonoBehaviour
+{
 
-	// Use this for initialization
-	public static ChangeRadius changeRadius;
+    // Use this for initialization
+    public static ChangeRadius changeRadius;
     private Vector3 mousePositon
-	{
-		get{
-			return Input.mousePosition;
-		}
-	} 
+    {
+        get
+        {
+            return Input.mousePosition;
+        }
+    }
 
 
     public Vector3 targetPosition;
 
-	private Vector3 movePosition;
+    private Vector3 movePosition;
 
-	public float delayTime=3f;
+    public float delayTime = 3f;
 
-	private TextRotation[] textRotation{
-		
-		get{
-			return GetComponentsInChildren<TextRotation>();
-		}
-	}
-    
+    private TextRotation[] textRotation
+    {
 
-	public bool Reading=false;
-
-	void Awake()
-	{
-		changeRadius=this;
-
-	}
-	void Start () {
-		
-		targetPosition=new Vector3(Screen.width/2,Screen.height/2,0);
-		//print(targetPosition);
-         Cursor.visible=false;
-
-	}
-	
-	// Update is called once per frame
-
-	
-	void Update () {
-           
-              if(Reading)
-			  
-				return;
-		movePosition=Vector3.Lerp(movePosition,mousePositon,Time.deltaTime*delayTime);
-
-         
-
-	   for(int i=0;i<textRotation.Length;i++)
-	   {
-		   textRotation[i].trueRadius=textRotation[i].radios*(movePosition-targetPosition).magnitude/1000;
-	   }
-     
+        get
+        {
+            return GetComponentsInChildren<TextRotation>();
+        }
+    }
 
 
-	//	print(movePosition);
-	}
+    public bool Reading = false;
 
-	public void ChangePos()
-	{
-		Reading=false;
-		float posx=Random.Range(0,Screen.width);
-		float posy=Random.Range(0,Screen.height);
-          
-		DOTween.To(() => targetPosition, x => targetPosition = x, new Vector3(posx,posy,0), 1f);
+    void Awake()
+    {
+        changeRadius = this;
 
-		print(posx+" "+posy);
-	}
+    }
+    void Start()
+    {
+
+        targetPosition = new Vector3(Screen.width / 2, Screen.height / 2, 0);
+        //print(targetPosition);
+    }
+
+    // Update is called once per frame
+
+
+    void Update()
+    {
+
+        if (Reading)
+
+            return;
+        movePosition = Vector3.Lerp(movePosition, mousePositon, Time.deltaTime * delayTime);
+
+
+
+        for (int i = 0; i < textRotation.Length; i++)
+        {
+            textRotation[i].trueRadius = textRotation[i].radios * (movePosition - targetPosition).magnitude / 1000;
+        }
+
+
+
+        //	print(movePosition);
+    }
+
+    public void ChangePos()
+    {
+        Reading = false;
+        float posx = Random.Range(0, Screen.width);
+        float posy = Random.Range(0, Screen.height);
+
+        DOTween.To(() => targetPosition, x => targetPosition = x, new Vector3(posx, posy, 0), 1f);
+
+        print(posx + " " + posy);
+    }
 }
